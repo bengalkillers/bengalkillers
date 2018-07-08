450ff265625f5832042a33d1712223b804a60987
bengalkillers <bengalkillers@users.noreply.github.com>
2018-07-08 21:15:28 +0530

    added changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+/*import '../../assets/js/main.js';*/
 
 declare var $:any;
 
@@ -11,11 +12,15 @@ export class AboutComponent implements OnInit {
 
   constructor() { }
 
+ 
+
   ngOnInit() {
 
   	// $ will be availble
   	/*isotope js starts*/
 		// external js: isotope.pkgd.js
+
+
 
 
 		// init Isotope
@@ -72,6 +77,44 @@ export class AboutComponent implements OnInit {
 		});
 		  
 		/*isotope ends*/
+
+	/*Vertical Timeline JS Starts*/
+
+	var timelineBlocks = $('.cd-timeline-block'),
+	offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) 
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+
+	/*Vertical Timeline JS Ends*/
+
+	//(timelines.length > 0) && initTimeline(timelines);
+
+	/*Hor corousel starts*/
+
+
+
+	/*Hor ends*/
+
 
   }
 
